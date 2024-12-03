7379b1ed54b25c574b5e273174509a370d36562e
ershoulanyan <111980481+ershoulanyan@users.noreply.github.com>
2024-12-03 16:17:13 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,19 +23,18 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'Bluess.store',
   HEO_HERO_TITLE_4: '使用方法',
   HEO_HERO_TITLE_5: '达西咖啡馆',
-  HEO_HERO_TITLE_LINK: 'https://www.notion.so/15184fd0387c802ca79ec5cb266ab441?pvs=4',
+  HEO_HERO_TITLE_LINK: 'https://www.bluess.store/article/%E5%9C%B0%E5%9D%80',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '原创', url: '/tag/原创' },
   HEO_HERO_CATEGORY_2: { title: '同人', url: '/tag/同人' },
-  HEO_HERO_CATEGORY_3: { title: '随笔', url: '/tag/随笔' },
-  HEO_HERO_CATEGORY_4: { title: '宇宙能量', url: '/tag/宇宙能量' },
+  HEO_HERO_CATEGORY_3: { title: '宇宙能量', url: '/tag/宇宙能量' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
