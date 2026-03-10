e992f656b255549b671c082b629e4f322187b4f2
ershoulanyan <111980481+ershoulanyan@users.noreply.github.com>
2026-03-10 21:32:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,56 +4,59 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2026-03-10', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'story of o', url: 'https://bloga4.bluess.store' },
+    { title: '二创作品集', url: 'https://bluess.store' },
+    { title: '欢迎来到我的个人站', url: 'https://next.bluess.store' }
   ],
 
   // 英雄区左右侧组件颠倒位置
-  HEO_HERO_REVERSE: false,
+  HEO_HERO_REVERSE: true,
   // 博客主体区左右侧组件颠倒位置
-  HEO_HERO_BODY_REVERSE: false,
+  HEO_HERO_BODY_REVERSE: true,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '长期主义',
+  HEO_HERO_TITLE_2: '终身学习',
+  HEO_HERO_TITLE_3: '渴望自由和金钱',
+  HEO_HERO_TITLE_4: '现充的高等算法',
+  HEO_HERO_TITLE_5: '21世纪拼搏打工人',
+  HEO_HERO_TITLE_LINK: 'https://bluess.store',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '小日常', url: '/tag/小日常' },
+  HEO_HERO_CATEGORY_2: { title: '创作论', url: '/tag/创作论' },
+  HEO_HERO_CATEGORY_3: { title: '教程分享', url: '/tag/教程分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  //   HERO_RECOMMEND_COVER: 'https://img.bluess.store/img/IMG_2669.JPG', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '💖 终身学习长期主义',
+    '💥 脚踏实地飞速进步',
+    '😺 创作艺术与床性恋',
+    '🌍 互联网野生探险员',
+    '💃 艺术人生追求者',
+    '🥞 五星级厨艺破坏者',
+    '🪐 热衷赚钱的梦想家',
+    '🪩 steam入库经济学'
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_ICON1: 'fa-solid fa-mug-hot',
+  HEO_INFO_CARD_URL2: 'https://qm.qq.com/q/WTsMADoqkc',
+  HEO_INFO_CARD_ICON2: 'fa-brands fa-qq',
+  HEO_INFO_CARD_URL3: 'https://ershoulanyan.github.io/LinkTree',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -116,7 +119,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
