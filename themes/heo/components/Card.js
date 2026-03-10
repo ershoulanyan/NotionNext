bedc3815f80f7c3a83ecc3724edddbc36710cf03
ershoulanyan <111980481+ershoulanyan@users.noreply.github.com>
2026-03-10 22:19:50 +0800

    Update Card.js

--- themes/heo/components/Card.js
+++ themes/heo/components/Card.js
@@ -1,9 +1,11 @@
-const Card = ({ children, headerSlot, className }) => {
-  return <div className={`${className || ''} card border dark:border-gray-700 rounded-xl lg:p-6 p-4`}>
-    <>{headerSlot}</>
-    <section>
-        {children}
-    </section>
-  </div>
+const Card = ({ children, headerSlot, className, ...rest }) => {
+  return (
+    <div
+      {...rest}
+      className={`${className || ''} card border dark:border-gray-700 rounded-xl lg:p-6 p-4`}>
+      <>{headerSlot}</>
+      <section>{children}</section>
+    </div>
+  )
 }
 export default Card
